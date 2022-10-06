3ac8fd16a849d1fc75dda0523f18cfd29816c4b5
Antonio Paulo Dambujo <pdambujo@gmail.com>
2022-10-06 10:55:33 +0200

    setup database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
+const connectDB = require("./config/database"); 
 
 // config
 dotenv.config({path:"backend/config/config.env"})
+
+// connect to database
+connectDB();
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
