4b7db65ff2291019251ef7d5aa5fbd12c7be9c88
Antonio Paulo Dambujo <pdambujo@gmail.com>
2022-10-10 17:36:49 +0200

    fix bug Auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -11,7 +11,7 @@ exports.isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded._id);
+    req.user = await User.findById(decoded.id);
 
     next();
 });
