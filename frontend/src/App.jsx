c8659362daf9ea238258e0d58cf73d5e73078ab9
Antonio Paulo Dambujo <pdambujo@gmail.com>
2022-10-13 11:48:56 +0200

    Initial Frontend, Navbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import { HomePage } from './pages';
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
